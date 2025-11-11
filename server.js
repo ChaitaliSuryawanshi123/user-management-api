8d7c34d5de510ba8f86de5a96e4981e9e4ff5fe8
Chaitali Suryawanshi <chaitali.suryawanshi@kanakasoftware.com>
2025-11-11 15:25:04 +0530

    Added dummy comment

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ require('dotenv').config();
 const cors = require('cors');
 const helmet = require('helmet');
 const userRoutes = require('./routes/userRoutes');
+//Some comment to create conflict
+//some comment to create conflict
 
 
+//some comment to create merge conflict
 app.use(express.json());  //built-in middleware to parse JSON bodies into req.body. Without this, req.body is undefined for JSON payloads
 app.use(cors());  //enables CORS : allows your frontend (on other origin) to communicate with the API. cors() accepts options for allowed origins, headers, credentials.
 app.use(helmet());  //sets security headers like X-Frame-Options, X-Content-Type-Options, Content-Security-Policy (helps guard against common attacks).
